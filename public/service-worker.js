983e321f9d72314a102740785d791d953989c4cb
Eli Pagan <mbk8689@gmail.com>
2022-02-20 15:50:12 -0500

    removed comments

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,10 +15,10 @@ self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
         caches.match(e.request).then(function (request) {
-            if (request) { // if cache is available, respond with cache
+            if (request) { 
                 console.log('responding with cache : ' + e.request.url)
                 return request
-            } else {       // if there are no cache, try fetching request
+            } else {      
                 console.log('file is not cached, fetching : ' + e.request.url)
                 return fetch(e.request)
             }
@@ -43,7 +43,7 @@ self.addEventListener('activate', function (e) {
             let cacheKeeplist = keyList.filter(function (key) {
                 return key.indexOf(APP_PREFIX);
             });
-            // add current cache name to keeplist
+          
             cacheKeeplist.push(CACHE_NAME);
 
             return Promise.all(
