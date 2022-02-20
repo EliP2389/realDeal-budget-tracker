11385bfef8611c5bcd823d8b917b1442fe5fd3a4
Eli Pagan <mbk8689@gmail.com>
2022-02-20 15:33:59 -0500

    indexedDB functional

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,66 +1,74 @@
 let db;
-const request = indexedDB.open('budget_tracker', 1);
+const request = indexedDB.open('real_budget_tracker', 1);
 
-request.onupgradedneeded = function(e) {
-    const db = e.target.result;
-    db.createObjectStore('budget_store', {autoIncrement: true});
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore('budget_tracker', { autoIncrement: true });
 };
 
-request.onsuccess = function(e) {
-    db = e.target.result;
+request.onsuccess = function (event) {
+
+    db = event.target.result;
+
 
     if (navigator.onLine) {
-    //  uploadBudgetTracker();
+        uploadPizza();
     }
 };
 
-request.onerror = function(e) {
-    console.log(e.target.errorCode);
+request.onerror = function (event) {
+
+    console.log(event.target.errorCode);
 };
 
 function saveRecord(record) {
-    const transaction = db.transaction(['budget_store'], 'readwrite');
+    const transaction = db.transaction(['budget_tracker'], 'readwrite');
 
-    const budgetObjectStore = transaction.objectStore('budget_store');
+    const pizzaObjectStore = transaction.objectStore('budget_tracker');
 
-    budgetObjectStore.add(record);
-};
+
+    pizzaObjectStore.add(record);
+}
 
 function uploadBudgetTracker() {
-    const transaction = db.transaction(['budget_store'], 'readwrite');
 
-    const budgetObjectStore = transaction.objectStore('budget_store');
+    const transaction = db.transaction(['budget_tracker'], 'readwrite');
 
-    const getAll = budgetObjectStore.getAll();
 
-    getAll.onsuccess = function() {
+    const pizzaObjectStore = transaction.objectStore('budget_tracker');
+
+
+    const getAll = pizzaObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+
         if (getAll.result.length > 0) {
-          fetch('/api/transaction/bulk', {
-            method: 'POST',
-            body: JSON.stringify(getAll.result),
-            headers: {
-              Accept: 'application/json, text/plain, */*',
-              'Content-Type': 'application/json'
-            }
-          })
-            .then(response => response.json())
-            .then(serverResponse => {
-              if (serverResponse.message) {
-                throw new Error(serverResponse);
-              }
-              const transaction = db.transaction(['budget_store'], 'readwrite');
-              
-              const budgetObjectStore = transaction.objectStore('budget_store');
-              
-              budgetObjectStore.clear();
-    
-              alert('All saved transactions has been submitted!');
+            fetch('/api/transactions/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
             })
-            .catch(err => {
-              console.log(err);
-            });
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+
+                    const transaction = db.transaction(['budget_tracker'], 'readwrite');
+                    const pizzaObjectStore = transaction.objectStore('budget_tracker');
+
+                    pizzaObjectStore.clear();
+                })
+                .catch(err => {
+
+                    console.log(err);
+                });
         }
-      };
-    
-};
+    };
+}
+
+
 window.addEventListener('online', uploadBudgetTracker);
